46718e42cb2be2dc9eb5d427f43067045978244b
Maikl Tadesse <mit@superdoc.ru>
2022-04-06 17:33:11 +0300

    prepublish things

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Answer from '@/components/Answer.vue'
 import Introduction from '@/components/Introduction.vue'
 import Question from '@/components/Question.vue'
+import Quizlet from '@/components/Quizlet.vue'
 import ProgressBar from '@/components/ProgressBar.vue'
 import Timer from '@/components/Timer.vue'
 
@@ -8,6 +9,7 @@ export {
   Answer,
   Introduction,
   Question,
+  Quizlet,
   ProgressBar,
   Timer
 }
